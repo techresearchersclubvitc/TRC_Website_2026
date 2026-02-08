16d3009f1001b42254f6fe45867845b423df293b
Aditya <aditya07.11.04.12@gmail.com>
2026-02-08 15:10:50 +0530

    Modified index.css and gitignore

---
+++ .vite/deps/react-dom_client.js
@@ -0,0 +1,8 @@
+import {
+  require_client
+} from "./chunk-VFPM35EC.js";
+import "./chunk-R3E4LXVO.js";
+import "./chunk-6YOVXODW.js";
+import "./chunk-P6RTVJOB.js";
+import "./chunk-G3PMV62Z.js";
+export default require_client();
